1eb17c37d0b92363dc73dd4585e948f65406ca87
zoushiqing <542920864@qq.com>
2019-03-15 13:33:22 +0800

    wo shi masater

--- app/src/main/java/com/demo/mygittest/MainActivity.kt
+++ app/src/main/java/com/demo/mygittest/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //fasdfasdfasf  fadf
+                //我是master
     }
 }
